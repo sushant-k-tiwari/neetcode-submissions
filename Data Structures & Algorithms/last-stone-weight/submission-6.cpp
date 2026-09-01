058b0986f83916f2ac3019d65304df5846db20ad
Sushant Kumar Tiwari <126879858+sushant-k-tiwari@users.noreply.github.com>
2026-09-01 19:37:01 +0530

    Add: last-stone-weight - submission-6

---
+++ Data Structures & Algorithms/last-stone-weight/submission-6.cpp
@@ -0,0 +1,19 @@
+class Solution {
+   public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> maxHeap;
+        for (int s : stones) {
+            maxHeap.push(s);
+        }
+        while (maxHeap.size() > 1) {
+            int first = maxHeap.top();
+            maxHeap.pop();
+            int second = maxHeap.top();
+            maxHeap.pop();
+
+            maxHeap.push(abs(first - second));
+        }
+        maxHeap.push(0);
+        return maxHeap.top();
+    }
+};
