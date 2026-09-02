49b1ee9c04bac1c5a756270a53deb02c52a95574
Sushant Kumar Tiwari <126879858+sushant-k-tiwari@users.noreply.github.com>
2026-09-02 21:09:03 +0530

    Add: first-unique-character-in-a-string - submission-0

---
+++ Data Structures & Algorithms/first-unique-character-in-a-string/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char, int> count;
+        for (char c : s) {
+            count[c]++;
+        }
+
+        for (int i = 0; i < s.size(); i++) {
+            if (count[s[i]] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
