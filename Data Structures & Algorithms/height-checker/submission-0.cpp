adfe91ac9336aeea0b540a9d2d47619e0a2d4ec7
Sushant Kumar Tiwari <126879858+sushant-k-tiwari@users.noreply.github.com>
2026-09-13 21:25:14 +0530

    Add: height-checker - submission-0

---
+++ Data Structures & Algorithms/height-checker/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+   public:
+    int heightChecker(vector<int>& heights) {
+        vector<int> sortedHeights;
+        for (int h : heights) {
+            sortedHeights.push_back(h);
+        }
+
+        sort(sortedHeights.begin(), sortedHeights.end());
+
+        int count = 0;
+        for (int i = 0; i < heights.size(); i++) {
+            if (heights[i] != sortedHeights[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+};
